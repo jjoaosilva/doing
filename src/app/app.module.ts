f0804465fcf367ee7e20cb5eacd9281be23dd896
Jose Joao <josejoaosilva.n@lit.ifce.edu.br>
2017-12-12 02:21:16 -0200

    Tela de cadastro
    Criação da tela de cadastro e mudanã de setRoot

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { CadastroPage } from '../pages/cadastro/cadastro';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    CadastroPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +21,8 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    CadastroPage
   ],
   providers: [
     StatusBar,
